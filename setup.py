fa7172818da1117a5f6feae7c49c841cb77d39a4
Olaf Gladis <olaf@gladis.org>
2015-05-21 20:43:43 +0200

    omg, a typo

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='smtplibaio',
     py_modules=['smtplibaio'],  # this must be the same as the name above
-    version='1.0.2',
-    description='A ansync version of smtplib',
+    version='1.0.3',
+    description='An async version of smtplib',
     author='Olaf Gladis',
     author_email='github@gladis.org',
     url='https://github.com/hwmrocker/smtplibaio',  # use the URL to the github repo
-    download_url='https://github.com/hwmrocker/smtplibaio/tarball/1.0.2',  # I'll explain this in a second
+    download_url='https://github.com/hwmrocker/smtplibaio/tarball/1.0.3',  # I'll explain this in a second
     keywords=['smtplib', 'asyncio'],  # arbitrary keywords
     classifiers=[
         "License :: OSI Approved :: MIT License",
