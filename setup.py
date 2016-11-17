f68f4e17936664ed275aa9bf943d85a2f2662460
Olaf Gladis <olaf.gladis@trivago.com>
2016-11-17 19:54:23 +0100

    bump version to 2.0.0

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='smtplibaio',
     py_modules=['smtplibaio'],  # this must be the same as the name above
-    version='1.0.4',
+    version='2.0.0',
     description='An async version of smtplib',
     author='Olaf Gladis',
     author_email='github@gladis.org',
     url='https://github.com/hwmrocker/smtplibaio',  # use the URL to the github repo
-    download_url='https://github.com/hwmrocker/smtplibaio/tarball/1.0.4',  # I'll explain this in a second
+    download_url='https://github.com/hwmrocker/smtplibaio/tarball/2.0.0',  # I'll explain this in a second
     keywords=['smtplib', 'asyncio'],  # arbitrary keywords
     classifiers=[
         "License :: OSI Approved :: MIT License",
